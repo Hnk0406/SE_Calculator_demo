0658890b8a8a3b8140b15a1991ff5130446a1b6a
Hariom N Kini <hnk0406@gmail.com>
2026-09-09 12:15:21 +0530

    change1

--- src/calculator.py
+++ src/calculator.py
@@ -3,12 +3,12 @@
 
 def add(a, b):
     """Add two numbers."""
-    return a + b
+    return a+b
 
 
 def subtract(a, b):
     """Subtract the second number from the first."""
-    return a - b
+    return a-b
 
 
 def multiply(a, b):
